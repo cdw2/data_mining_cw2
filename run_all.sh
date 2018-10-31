9eb3b6716ce0b292715158fd2c79d4faae4022a3
Chris Walsh <cwalsh314@gmail.com>
2018-10-31 13:15:05 +0000

    automation script created

--- run_all.sh
+++ run_all.sh
@@ -5,31 +5,45 @@ arff_folder="fer2018/arffs"
 reduction_folder="fer2018/reduced_arffs"
 transformed_arffs="fer018/transformed_arffs"
 
-# rm $arff_folder/*
+cwd=$(pwd)
 
-##Convert to ARFF
-# for file in $csv_folder/*; do
-#     echo "$file";
-#     if [ "$file" = "$csv_folder/fer2018.csv" ];then
-#     ./csv_arff.py "$file"
-#     else
-#     ./csv_arff.py --skip "$file"
-#     fi
-# done;
+rm $arff_folder/*
 
-# #Reduce Data
-# for file in $arff_folder/*; do
-#     echo "$file";
-#     ./part3_attributeReduction.py "$file"
-# done;
+#Convert to ARFF
+for file in $csv_folder/*; do
+    echo "$file";
+    if [ "$file" = "$csv_folder/fer2018.csv" ];then
+    ./csv_arff.py "$file"
+    else
+    ./csv_arff.py --skip "$file"
+    fi
+done;
+
+#Reduce Data
+for file in $arff_folder/*; do
+    echo "$file";
+    ./part3_attributeReduction.py "$file"
+done;
 
 #extract 70 pixels
+echo "Starting Pixel Extraction:";
+
 ./extract_pixels.py pixel_values/pixel_values_14.txt
 ./extract_pixels.py pixel_values/pixel_values_35.txt
 ./extract_pixels.py pixel_values/pixel_values_70.txt
 
-echo "Pixel Extraction Complete for all file";
+echo "Pixel Extraction Complete for all files";
 
+
+echo "Creating Test/Training Files:";
+cd fer2018/transformed_arffs
+python3 TestTrain.py transformed_14.arff
+python3 TestTrain.py transformed_35.arff
+python3 TestTrain.py transformed_70.arff
+mv Testtransformed* ../split/
+mv Training* ../split/
+echo "Done"
+cd "$cwd"
 
 
 #run weka
